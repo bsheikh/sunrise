3ef30793a227c6fdf30da5f215dde70b1c5464af
Bill Sheikh <bsheikh@adparlor.com>
2017-10-23 23:32:28 -0400

    added quality env

--- config/environments.config.js
+++ config/environments.config.js
@@ -13,6 +13,13 @@ module.exports = {
   }),
 
   // ======================================================
+  // Overrides when NODE_ENV === 'quality'
+  // ======================================================
+  quality: (config) => ({
+    compiler_public_path: `http://${config.server_host}:${config.server_port}/`
+  }),
+
+  // ======================================================
   // Overrides when NODE_ENV === 'production'
   // ======================================================
   sandbox: (config) => ({
