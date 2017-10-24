e882fef1693d259b0472282b82bdcf8721fc643c
Bill Sheikh <bsheikh@adparlor.com>
2017-10-23 23:46:46 -0400

    made linting pass

--- src/modules/LandingPage/components/LandingPage.js
+++ src/modules/LandingPage/components/LandingPage.js
@@ -6,7 +6,7 @@ export default class LandingPage extends Component {
   render() {
     return (
       <div className="LandingPage__Component">
-        <h1>don't wait for the sunrise to start the day.</h1>
+        <h1>{"don't wait for the sunrise to start the day."}</h1>
       </div>
     );
   }
